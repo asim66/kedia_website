23b2105a3af5848cf9b926e0586674733bc052b5
Asim Jena <jena.asim@gmail.com>
2026-08-17 10:21:23 +0530

    refactor: remove API path disallowance from robots.txt configuration

--- app/robots.ts
+++ app/robots.ts
@@ -7,9 +7,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
+
+
 
